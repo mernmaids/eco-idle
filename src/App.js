05141cfd6547620e4370325fcece041096ab040a
centipeda <joshua.ka.cepeda@gmail.com>
2022-02-25 19:46:15 -0500

    start transfer to webpack build

--- src/App.js
+++ src/App.js
@@ -1,43 +1,14 @@
-import {
-  html,
-  render,
-  useState,
-  useEffect,
-} from "https://unpkg.com/htm/preact/standalone.module.js";
+import './App.css';
 
-import { getData } from "./Api.js";
-import { Menu } from './components/menu/Menu.js';
-import { ContentView } from './components/ContentView.js';
-
-// Parent component
 function App() {
-    const [data, setData] = useState();
-    const [view, setView] = useState('sector');
-    function handleMenuSelect(selection) {
-        if(view !== setView) {
-            setView(selection);
-        }
-    }
-
-    // only gets save data at first render
-    useEffect(() => {
-        getData().then((d) => {
-            setData(d);
-        });
-    }, []);
-
-    // if data is still loading
-    if(!data) {
-        return html`<div>Loading...</div>`;
-    }
-
-    // if data is loaded
-    return html`
-        <div class="h-screen">
-            <${Menu} onMenuSelect=${handleMenuSelect} options=${data.menuOptions}/>
+  return (
+    <div className="h-screen">
+        {/*
+            <Menu onMenuSelect=${handleMenuSelect} options=${data.menuOptions}/>
             <${ContentView} data=${data} view=${view}/>
-        </div>
-    `;
+        */}
+    </div>
+  );
 }
 
-render(html` <${App}/> `, document.getElementById("app"));
+export default App;
