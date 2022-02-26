ebac1c2a92079f7f6924d60c0a0a94ec02e84eda
Tanner Waltz <tannerwaltz99@gmail.com>
2022-02-25 22:24:13 -0500

    started parse implementation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import './App.css';
+import { Menu } from './components/menu/Menu';
+import { ContentView } from './components/ContentView.js';
+import { useEffect, useState } from 'react';
+import { getData } from './services/Api.js';
+import * as Env from "./environments";
+import Parse from "parse";
+
+Parse.initialize(Env.APPLICATION_ID, Env.JAVASCRIPT_KEY);
+Parse.serverURL = Env.SERVER_URL;
 
 function App() {
-  return (
-    <div className="h-screen">
-        {/*
-            <Menu onMenuSelect=${handleMenuSelect} options=${data.menuOptions}/>
-            <${ContentView} data=${data} view=${view}/>
-        */}
-    </div>
-  );
+    const [data, setData] = useState();
+    const [view, setView] = useState('sector');
+    function handleMenuSelect(selection) {
+        if(view !== setView) {
+            setView(selection);
+        }
+    }
+
+    // only gets save data at first render
+    useEffect(() => {
+        getData().then((d) => {
+            setData(d);
+        });
+    }, []);
+
+    if(!data) {
+        return (<div>Loading...</div>);
+    }
+
+    return (
+        <div className="h-screen">
+                <Menu onMenuSelect={handleMenuSelect} options={data.menuOptions}/>
+                <ContentView data={data} view={view}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/environments.js
@@ -0,0 +1,5 @@
+module.exports = {
+    APPLICATION_ID: "aRQ9v17krde0snw6YEEmOjcEDf1K4gk4LFcGKpgL",
+    JAVASCRIPT_KEY: "GHXIwIjp1FHd5qY6BatFtERIFmkKpz4TUpGyZ1kv",
+    SERVER_URL: "https://parseapi.back4app.com/"
+  };
